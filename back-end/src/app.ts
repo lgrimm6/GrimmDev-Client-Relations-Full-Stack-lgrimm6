87b374d3f8e6193b34fb01902c4aa2ed0359e4db
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-01 18:14:25 -0300

    Feat: middleware AppError

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,9 +1,23 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { AppError } from "./errors/appError";
 import userRouter from "./router/users.routes";
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+app.use((err: Error, Request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+  console.error(err);
+  return response
+    .status(500)
+    .json({ status: "error", message: "Internal server error" });
+});
 
 export default app;
