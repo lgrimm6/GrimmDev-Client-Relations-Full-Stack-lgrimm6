29ff721d9fe4605b7e54f4a0a210b6a1d59d17c3
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-01 23:26:47 -0300

    Feat: delete user route

--- back-end/src/router/userRoutes/users.routes.ts
+++ back-end/src/router/userRoutes/users.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import createUserController from "../../controllers/users/createUser.controller";
+import deleteUserController from "../../controllers/users/deleteUser.controller";
+import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
 
 const router = Router();
 
 router.post("", createUserController);
+router.delete("/:uuid", verifyIdParams, deleteUserController);
 
 export default router;
