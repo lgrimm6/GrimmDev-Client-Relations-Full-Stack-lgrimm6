8674e8ab119700249027f1f4b1165269b5f09953
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-02 12:41:19 -0300

    Feat: app use to route contacts

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,11 +2,13 @@ import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import { AppError } from "./errors/appError";
 import userRouter from "./router/userRoutes/users.routes";
+import contactRouter from "./router/contactRoutes/contact.routes";
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", userRouter);
+app.use("/users/contact", contactRouter);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
