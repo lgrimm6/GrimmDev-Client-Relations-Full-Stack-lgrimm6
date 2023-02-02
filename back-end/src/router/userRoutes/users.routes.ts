a705d8b9584a074486584edd6b770f0f99f0779d
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-02 12:38:32 -0300

    Feat: added token validation in route

--- back-end/src/router/userRoutes/users.routes.ts
+++ back-end/src/router/userRoutes/users.routes.ts
@@ -3,13 +3,29 @@ import createUserController from "../../controllers/users/createUser.controller"
 import deleteUserController from "../../controllers/users/deleteUser.controller";
 import listUserController from "../../controllers/users/listUser.controller";
 import updateUserController from "../../controllers/users/updateUser.controller";
+import verifyAuthTokenMiddleware from "../../middlewares/verifyAuthToken.middlewares";
 import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
 
 const router = Router();
 
 router.post("", createUserController);
-router.get("/:uuid", verifyIdParams, listUserController);
-router.patch("/:uuid", verifyIdParams, updateUserController);
-router.delete("/:uuid", verifyIdParams, deleteUserController);
+router.get(
+  "/:uuid",
+  verifyIdParams,
+  verifyAuthTokenMiddleware,
+  listUserController
+);
+router.patch(
+  "/:uuid",
+  verifyIdParams,
+  verifyAuthTokenMiddleware,
+  updateUserController
+);
+router.delete(
+  "/:uuid",
+  verifyIdParams,
+  verifyAuthTokenMiddleware,
+  deleteUserController
+);
 
 export default router;
