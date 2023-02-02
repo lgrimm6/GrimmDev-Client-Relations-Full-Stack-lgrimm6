e48b978399c714467f7a923bed398a62e1529f2e
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-02 01:18:11 -0300

    Feat: update user route

--- back-end/src/router/userRoutes/users.routes.ts
+++ back-end/src/router/userRoutes/users.routes.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import createUserController from "../../controllers/users/createUser.controller";
 import deleteUserController from "../../controllers/users/deleteUser.controller";
 import listUserController from "../../controllers/users/listUser.controller";
+import updateUserController from "../../controllers/users/updateUser.controller";
 import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
 
 const router = Router();
 
 router.post("", createUserController);
 router.get("/:uuid", verifyIdParams, listUserController);
+router.patch("/:uuid", verifyIdParams, updateUserController);
 router.delete("/:uuid", verifyIdParams, deleteUserController);
 
 export default router;
