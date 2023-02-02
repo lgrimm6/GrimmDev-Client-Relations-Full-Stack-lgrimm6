b3277579c87de3d38139c4172ca00071d83cc139
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-02 00:25:31 -0300

    Feat: List user by id router

--- back-end/src/router/userRoutes/users.routes.ts
+++ back-end/src/router/userRoutes/users.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import createUserController from "../../controllers/users/createUser.controller";
 import deleteUserController from "../../controllers/users/deleteUser.controller";
+import listUserController from "../../controllers/users/listUser.controller";
 import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
 
 const router = Router();
 
 router.post("", createUserController);
+router.get("/:uuid", verifyIdParams, listUserController);
 router.delete("/:uuid", verifyIdParams, deleteUserController);
 
 export default router;
