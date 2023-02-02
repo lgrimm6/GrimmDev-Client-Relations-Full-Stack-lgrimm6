e9e3a8385d21133b2c88af5e020c106573c02ef6
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-02 12:34:15 -0300

    Feat: middleware token validation

--- back-end/src/middlewares/verifyAuthToken.middlewares.ts
+++ back-end/src/middlewares/verifyAuthToken.middlewares.ts
@@ -1,1 +1,30 @@
 import { NextFunction, Request, Response } from "express";
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+
+const verifyAuthTokenMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let token: string | undefined = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "Missing authorization headers" });
+  }
+  if (token.includes("Bearer")) {
+    token = token.split(" ")[1];
+  }
+  jwt.verify(token, process.env.SECRET_KEY as string, (error, decoded: any) => {
+    if (error) {
+      return res.status(401).json({
+        message: "Unauthorized",
+      });
+    }
+    req.user = {
+      id: decoded.id,
+    };
+    return next();
+  });
+};
+
+export default verifyAuthTokenMiddleware;
