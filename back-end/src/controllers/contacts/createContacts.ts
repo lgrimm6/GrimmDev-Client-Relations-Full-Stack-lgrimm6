85e666c68c959dc1f5f81072516d05e7b3576e6e
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 18:55:35 -0300

    Fix: userId renamed to clientIdToken

--- back-end/src/controllers/contacts/createContacts.ts
+++ back-end/src/controllers/contacts/createContacts.ts
@@ -2,9 +2,12 @@ import { Request, Response } from "express";
 import createContactServices from "../../services/contacts/createContact.services";
 
 const createContactController = async (req: Request, res: Response) => {
-  const userId: string = req.user.id;
+  const clientIdToken: string = req.user.id;
   const contactData = req.body;
-  const contactCreated = await createContactServices(userId, contactData);
+  const contactCreated = await createContactServices(
+    clientIdToken,
+    contactData
+  );
 
   return res
     .status(201)
