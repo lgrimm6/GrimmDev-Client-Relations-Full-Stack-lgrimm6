0ef50b40546bbe80b978ecb3651bb985d8b43f05
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 18:56:49 -0300

    Feat: controller function to list contacts

--- back-end/src/controllers/contacts/listContacts.ts
+++ back-end/src/controllers/contacts/listContacts.ts
@@ -1,3 +1,10 @@
-const listContactController = async () => {};
+import { Request, Response } from "express";
+import listContactsServices from "../../services/contacts/listContacts.services";
+
+const listContactController = async (req: Request, res: Response) => {
+  const clientId: string = req.params.clientId;
+  const contacts = await listContactsServices(clientId);
+  return res.json({ contacts });
+};
 
 export default listContactController;
