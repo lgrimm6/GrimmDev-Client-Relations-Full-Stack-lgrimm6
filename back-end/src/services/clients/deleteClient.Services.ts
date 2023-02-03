ef81c067b77b0b01d5cda91243e7c0b2ae624af2
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 19:20:08 -0300

    Fix: uuid renamed to clientId

--- back-end/src/services/clients/deleteClient.Services.ts
+++ back-end/src/services/clients/deleteClient.Services.ts
@@ -3,17 +3,17 @@ import AppDataSource from "../../data-source";
 import { Client } from "../../entities/client.entity";
 import { AppError } from "../../errors/appError";
 
-const deleteClientServices = async (uuid: string): Promise<void> => {
+const deleteClientServices = async (clientId: string): Promise<void> => {
   const clientRepository: Repository<Client> =
     AppDataSource.getRepository(Client);
   const deleteClient: Client | null = await clientRepository.findOneBy({
-    id: uuid,
+    id: clientId,
   });
 
   if (!deleteClient) {
     throw new AppError(404, "client ID not found");
   }
-  await clientRepository.delete({ id: uuid });
+  await clientRepository.delete({ id: clientId });
 
   return;
 };
