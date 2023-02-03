7e246a39df778fe68097e192d9fd9dcad3d17fe0
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 19:20:27 -0300

    Fix: uuid renamed to clientId

--- back-end/src/services/clients/listClient.services.ts
+++ back-end/src/services/clients/listClient.services.ts
@@ -3,10 +3,12 @@ import AppDataSource from "../../data-source";
 import { Client } from "../../entities/client.entity";
 import { AppError } from "../../errors/appError";
 
-const listClientServices = async (uuid: string) => {
+const listClientServices = async (clientId: string) => {
   const clientRepository: Repository<Client> =
     AppDataSource.getRepository(Client);
-  const client: Client | null = await clientRepository.findOneBy({ id: uuid });
+  const client: Client | null = await clientRepository.findOneBy({
+    id: clientId,
+  });
   if (!client) {
     throw new AppError(404, "Client does not exist");
   }
