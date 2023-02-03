fd7029de287b8dfe0f41d10e007132d6a232b3bf
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 19:18:04 -0300

    Feat: router get and patch contact

--- back-end/src/router/contactRoutes/contact.routes.ts
+++ back-end/src/router/contactRoutes/contact.routes.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
 import createContactController from "../../controllers/contacts/createContacts";
+import listContactController from "../../controllers/contacts/listContacts";
+import updateContactController from "../../controllers/contacts/updateContact.controller";
 import verifyAuthTokenMiddleware from "../../middlewares/verifyAuthToken.middlewares";
+import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
 
 const router = Router();
 
-router.post("", verifyAuthTokenMiddleware, createContactController);
+router.post("/contact", verifyAuthTokenMiddleware, createContactController);
+router.get(
+  "/:clientId/contact",
+  verifyIdParams,
+  verifyAuthTokenMiddleware,
+  listContactController
+);
+router.patch(
+  "/:clientId/contact/:contactId",
+  verifyIdParams,
+  verifyAuthTokenMiddleware,
+  updateContactController
+);
 
 export default router;
