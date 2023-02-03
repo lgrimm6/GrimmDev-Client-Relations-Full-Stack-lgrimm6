499688dfd09eeb4e0755857362e2df872fa5dd4d
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 19:03:30 -0300

    Feat: validation for contact id

--- back-end/src/middlewares/verifyIdParams.middlewares.ts
+++ back-end/src/middlewares/verifyIdParams.middlewares.ts
@@ -6,12 +6,21 @@ const verifyIdParams = (
   res: Response,
   next: NextFunction
 ): void => {
-  const uuid = req.params.uuid;
   const regexUuid =
     /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/;
-  const isRegex = regexUuid.test(uuid);
-  if (!isRegex) {
-    throw new AppError(400, "id must be a valid uuid");
+  if (req.params.clientId) {
+    const clientId = req.params.clientId;
+    const isRegex = regexUuid.test(clientId);
+    if (!isRegex) {
+      throw new AppError(400, "id must be a valid uuid");
+    }
+  }
+  if (req.params.contactId) {
+    const contactId = req.params.contactId;
+    const isRegex = regexUuid.test(contactId);
+    if (!isRegex) {
+      throw new AppError(400, "id must be a valid uuid");
+    }
   }
   return next();
 };
