1186f93b3e61f2f24097b2087d85b4f24293ff3b
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 21:58:24 -0300

    Feat: add middleware is the owner in router

--- back-end/src/router/contactRoutes/contact.routes.ts
+++ back-end/src/router/contactRoutes/contact.routes.ts
@@ -4,6 +4,7 @@ import listContactController from "../../controllers/contacts/listContacts";
 import updateContactController from "../../controllers/contacts/updateContact.controller";
 import verifyAuthTokenMiddleware from "../../middlewares/verifyAuthToken.middlewares";
 import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
+import verifyIsOwnerMiddleware from "../../middlewares/verifyIsOwner.middlewares";
 
 const router = Router();
 
@@ -12,13 +13,21 @@ router.get(
   "/:clientId/contact",
   verifyIdParams,
   verifyAuthTokenMiddleware,
+  verifyIsOwnerMiddleware,
   listContactController
 );
 router.patch(
   "/:clientId/contact/:contactId",
   verifyIdParams,
   verifyAuthTokenMiddleware,
+  verifyIsOwnerMiddleware,
   updateContactController
+);
+router.delete(
+  "/:clientId/contact/:contactId",
+  verifyIdParams,
+  verifyAuthTokenMiddleware,
+  verifyIsOwnerMiddleware
 );
 
 export default router;
