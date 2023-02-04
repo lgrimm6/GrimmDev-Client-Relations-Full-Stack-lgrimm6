96e36332e43c6ad54f8a8c9b0cea9e62fc57d842
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-03 23:38:45 -0300

    Feat: route delete client contact

--- back-end/src/router/contactRoutes/contact.routes.ts
+++ back-end/src/router/contactRoutes/contact.routes.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import createContactController from "../../controllers/contacts/createContacts";
 import listContactController from "../../controllers/contacts/listContacts";
 import updateContactController from "../../controllers/contacts/updateContact.controller";
+import deleteContactController from "../../controllers/contacts/deleteContact.controller";
 import verifyAuthTokenMiddleware from "../../middlewares/verifyAuthToken.middlewares";
 import verifyIdParams from "../../middlewares/verifyIdParams.middlewares";
 import verifyIsOwnerMiddleware from "../../middlewares/verifyIsOwner.middlewares";
+import verifyUpdateData from "../../middlewares/verifyUpdateData.middlewares";
 
 const router = Router();
 
@@ -21,13 +23,15 @@ router.patch(
   verifyIdParams,
   verifyAuthTokenMiddleware,
   verifyIsOwnerMiddleware,
+  verifyUpdateData,
   updateContactController
 );
 router.delete(
   "/:clientId/contact/:contactId",
   verifyIdParams,
   verifyAuthTokenMiddleware,
-  verifyIsOwnerMiddleware
+  verifyIsOwnerMiddleware,
+  deleteContactController
 );
 
 export default router;
