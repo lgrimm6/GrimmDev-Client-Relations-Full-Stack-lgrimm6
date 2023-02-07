e00c3544050fbceeed343d59116f3b9b548d7857
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-07 01:42:13 -0300

    Feat: add router /profile

--- back-end/src/router/login/login.routes.ts
+++ back-end/src/router/login/login.routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 
 import loginController from "../../controllers/login/loginController";
+import profileController from "../../controllers/login/profile.controller";
+import verifyAuthTokenMiddleware from "../../middlewares/verifyAuthToken.middlewares";
 
 const router = Router();
-router.post("", loginController);
+router.post("/login", loginController);
+router.get("/profile", verifyAuthTokenMiddleware, profileController);
 
 export default router;
