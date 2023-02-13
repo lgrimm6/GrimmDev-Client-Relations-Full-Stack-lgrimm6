dc51913855d36929307e7ae2ec362d2750e2b92e
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-02-12 23:01:11 -0300

    Fix: appdatasource configured to manually import entities and migrations

--- back-end/src/data-source.ts
+++ back-end/src/data-source.ts
@@ -1,6 +1,10 @@
 import { DataSource } from "typeorm";
-
 import "dotenv/config";
+
+import { Client } from "./entities/client.entity";
+import { Contact } from "./entities/contact.entity";
+import { initialMigration1676252484833 } from "./migrations/1676252484833-initialMigration";
+import { createTables1676252489796 } from "./migrations/1676252489796-createTables";
 
 const AppDataSource = new DataSource({
   type: "postgres",
@@ -11,8 +15,8 @@ const AppDataSource = new DataSource({
   database: process.env.POSTGRES_DB,
   synchronize: false,
   logging: true,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migrations/*.ts"],
+  entities: [Client, Contact],
+  migrations: [initialMigration1676252484833, createTables1676252489796],
 });
 
 export default AppDataSource;
